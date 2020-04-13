08417dce4f95a1b34858e891d6f31d9f14fd3dac
arafatreza <arafat.reza1997@gmail.com>
2020-04-14 03:56:09 +0600

    Product Type Index and Create Added

---
+++ OnlineShop/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data;
+using OnlineShop.Models;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public ProductTypesController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+           // var data = _applicationDbContext.ProductTypes.ToList();
+            return View(_applicationDbContext.ProductTypes.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if(ModelState.IsValid)
+            {
+                _applicationDbContext.ProductTypes.Add(productTypes);
+                await _applicationDbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes);
+        }
+    }
+}
